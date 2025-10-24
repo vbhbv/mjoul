a6a3b9e429d074a417776f539febde7d165f3782
mjwl <72283979+vbhbv@users.noreply.github.com>
2025-10-25 00:56:13 +0300

    Update __init__.py

--- EthiCore/__init__.py
+++ EthiCore/__init__.py
@@ -1,1 +1,14 @@
 
+# EthiCore/__init__.py
+# لجعل المجلد EthiCore قابلاً للاستيراد كمكتبة Python.
+
+from .InferenceEngine import InferenceEngine
+from .EthicalNetsModel import EthicalNetsModel
+from .ContextualClassifier import ContextualClassifier
+
+# تحديد ما يتم استيراده تلقائيًا عند استخدام 'from EthiCore import *'
+__all__ = [
+    "InferenceEngine",
+    "EthicalNetsModel",
+    "ContextualClassifier",
+]
